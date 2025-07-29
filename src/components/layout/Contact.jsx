1ec642403cc217eeae9bfe281ab875be6fb33dab
Marc Doncel <kramleknod@gmail.com>
2025-07-29 14:59:57 +0200

    Add Netlify forms

--- src/components/layout/Contact.jsx
+++ src/components/layout/Contact.jsx
@@ -6,15 +6,15 @@ export default function Contact() {
       <form name="contact" action="#" className="flex flex-col gap-8" netlify>
         <div>
           <label htmlFor="email" className="block mb-2">Your email</label>
-          <input type="email" id="email" className="block p-2.5 w-full text-sm bg-gray-50 border border-gray-300 rounded-lg shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="your@email.com" required />
+          <input type="email" id="email" name="email" className="block p-2.5 w-full text-sm bg-gray-50 border border-gray-300 rounded-lg shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="your@email.com" required />
         </div>
         <div>
           <label htmlFor="subject" className="block mb-2">Subject</label>
-          <input type="text" id="subject" className="block p-3 w-full text-sm bg-gray-50 border border-gray-300 rounded-lg shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Let me know how I can help you" required />
+          <input type="text" id="subject" name="subject" className="block p-3 w-full text-sm bg-gray-50 border border-gray-300 rounded-lg shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Let me know how I can help you" required />
         </div>
         <div className="sm:col-span-2">
           <label htmlFor="message" className="block mb-2">Your message</label>
-          <textarea id="message" rows="6" className="block p-2.5 w-full text-sm bg-gray-50 border border-gray-300 rounded-lg shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+          <textarea id="message" name="message" rows="6" className="block p-2.5 w-full text-sm bg-gray-50 border border-gray-300 rounded-lg shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Leave a comment..."></textarea>
         </div>
         <button type="submit" className="py-3 px-5 sm:w-fit text-center text-gray-100 bg-neutral-900 rounded-lg cursor-pointer hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300">Send message</button>
       </form>
