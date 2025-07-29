1c674225e2a2ff3f28c157cf7a25922ac5856aab
Marc Doncel <kramleknod@gmail.com>
2025-07-29 17:29:14 +0200

    Add formsubmit.co forms service

--- src/app/thank-you/page.jsx
+++ src/app/thank-you/page.jsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 import ReturnHomeIcon from '@/components/icons/ReturnHomeIcon';
 
-export default function NotFound() {
+export default function ThankYouPage() {
   return (
     <div className="flex flex-col gap-8 items-center justify-center px-4 h-[calc(100dvh-128px)] text-center">
-      <h1 className="text-5xl font-bold">404 - Page not found</h1>
-      <p className="text-lg text-gray-600">Sorry, the page you’re looking for doesn’t exist or has been moved.</p>
+      <h1 className="text-5xl font-bold">Thank you</h1>
+      <p className="text-lg text-gray-600">We've received your message and will reply shortly.</p>
       <Link href={"/"}>
         <div className="flex gap-2 px-4 py-2 border border-gray-900 rounded-4xl">
           <ReturnHomeIcon /> Go to homepage
